6bd15126be2c12ef074e3457a6fb0f40699d157f
Anny27-pixel <jabintasmin102523@gmail.com>
2024-12-28 20:46:53 +0600

    create a view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def myfunctioncall(request):
+    return HttpResponse("Hello world")
+
