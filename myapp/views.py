90fc2395bf308ad9d220978bfcd0c8e8dfb71c8c
Anny27-pixel <jabintasmin102523@gmail.com>
2024-12-28 20:57:30 +0600

    add two int and view

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def myfunctioncall(request):
     return HttpResponse("Hello world")
+def myfunctionabout(request):
+    return HttpResponse("I am Jabin Tasmin Anny")
+
+def add(request,a,b):
+    return HttpResponse(a+b)
 
